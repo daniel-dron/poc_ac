dd99b11809bad9e8a2a3a0b331c744446fbef122
Daniel Dron <daniel.dron@caixamagica.pt>
2023-04-22 21:53:40 +0100

    add missing header

---
+++ sv_standalone_client/log.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#define log_error(...) printf("[ERROR!!]: " __VA_ARGS__)
+#define log_trace(...) printf("[TRACE]: " __VA_ARGS__)
+#define log_info(...) printf("[INFO]: " __VA_ARGS__)
