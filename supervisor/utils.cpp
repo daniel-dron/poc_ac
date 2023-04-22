8688bd0f3bd42a935bc99d2a2ce221f2a6ad16bc
Daniel Dron <daniel.dron@caixamagica.pt>
2023-04-23 00:25:37 +0100

    hot check loaded modules

--- supervisor/utils.cpp
+++ supervisor/utils.cpp
@@ -2,55 +2,36 @@
 #include "utils.h"
 
 namespace utils {
-pmod_info g_modules{};
-size_t g_number_of_modules{};
 
 bool in_valid_module(UINT64 addr)
 {
-    if (!g_modules) {
-        ULONG size{};
-        ZwQuerySystemInformation(SystemModuleInformation, nullptr, 0, &size);
+    ULONG size{};
+    ZwQuerySystemInformation(SystemModuleInformation, nullptr, 0, &size);
 
-        if (!size) {
-            return false;
-        }
-
-        _RTL_PROCESS_MODULES *modules =
-            reinterpret_cast<_RTL_PROCESS_MODULES *>(
-                ExAllocatePoolZero(NonPagedPool, size, 'RMT'));
-        if (!modules) {
-            return false;
-        }
-
-        ZwQuerySystemInformation(SystemModuleInformation, modules, size, &size);
-
-        g_modules = reinterpret_cast<pmod_info>(ExAllocatePoolZero(
-            NonPagedPool, sizeof(mod_info) * modules->NumberOfModules, ' mvi'));
-
-        KdLog("Loaded %ld modules!\n", modules->NumberOfModules);
-        g_number_of_modules = modules->NumberOfModules;
-        for (ULONG i = 0; i < modules->NumberOfModules; i++) {
-            g_modules[i] = mod_info{
-                reinterpret_cast<UINT64>(modules->Modules[i].ImageBase),
-                modules->Modules[i].ImageSize};
-            KdLog("%s - [0x%p - 0x%p]\n", modules->Modules[i].FullPathName,
-                  g_modules[i].base, g_modules[i].size);
-        }
-
-        ExFreePool(modules);
-
-        KdLog("Initialized g_modules\n");
+    if (!size) {
+        return false;
     }
 
-    bool found = false;
+    _RTL_PROCESS_MODULES *modules = reinterpret_cast<_RTL_PROCESS_MODULES *>(
+        ExAllocatePoolZero(NonPagedPool, size, 'RMT'));
+    if (!modules) {
+        return false;
+    }
 
-    for (ULONG i = 0; i < g_number_of_modules; i++) {
-        if (addr >= g_modules[i].base &&
-            (addr + 0xFFF) <= (g_modules[i].base + g_modules[i].size)) {
+    ZwQuerySystemInformation(SystemModuleInformation, modules, size, &size);
+
+    bool found = false;
+    for (ULONG i = 0; i < modules->NumberOfModules; i++) {
+        if (addr >= reinterpret_cast<UINT64>(modules->Modules[i].ImageBase) &&
+            (addr + 0xFFF) <=
+                (reinterpret_cast<UINT64>(modules->Modules[i].ImageBase) +
+                 modules->Modules[i].ImageSize)) {
             found = true;
             break;
         }
     }
+
+    ExFreePool(modules);
 
     return found;
 }
